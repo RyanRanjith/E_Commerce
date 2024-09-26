372aa7474ae251c6da1e5876c229038948dc592b
RyanRanjith <kingranjith4@gmail.com>
2024-09-26 12:20:24 -0700

    a

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,12 +6,16 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 import Product from './pages/Product'
 import Cart from './pages/Cart'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
+import Orders from './pages/Orders'
+import Navbar from './components/Navbar'
 
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
-      
+      <Navbar/>
       <Routes>
        <Route path='/' element = {<Home/>} />
        <Route path='/collection' element = {<Collection/>} />
@@ -19,6 +23,9 @@ const App = () => {
        <Route path='/contact' element = {<Contact/>} />
        <Route path='/product/:productId' element = {<Product/>} />
        <Route path='/cart' element = {<Cart/>} />
+       <Route path='/login' element = {<Login/>} />
+       <Route path='/place-order' element = {<PlaceOrder/>} />
+       <Route path='/orders' element = {<Orders/>} />
        
 
       </Routes>

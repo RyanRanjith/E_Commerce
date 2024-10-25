39fdee35f3f013d9f54b8a56ba4dde206a951856
RyanRanjith <kingranjith4@gmail.com>
2024-10-24 18:58:27 -0700

    a

--- README.md
+++ README.md
@@ -24,3 +24,5 @@ October 17 - 8:01:03 - Login.jsx  8:15:31 at Add.jsx line 12
 October 18 - 8:30:10 - Add.jsx line 84 , 8:46:04 at Add.jsx line 86
 
 October 21 - 9:0029 - Add.jsx line 43
+
+October 23 - 9:32:38 - Fronend - BestSeller.jsx am  
--- admin/src/pages/List.jsx
+++ admin/src/pages/List.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { backendUrl, currency } from '../App'
 import { toast } from 'react-toastify'
 
-const List = () => {
+const List = ({token}) => {
 
 const [list,setList] = useState([])
 
@@ -22,6 +22,21 @@ try {
   toast.error(error.message)
   
  }
+}
+
+const removeProduct = async (id) => {
+  try {
+    const response = await axios.post(backendUrl + '/api/product/remove', {id}, {headers:{token}})
+    if (response.data.success) {
+      toast.success(response.data.message)
+      await fetchList();
+    } else {
+      toast.error(response.data.message)
+    }
+  } catch (error) {
+    console.log(error);
+    toast.error(error.message)
+  }
 }
 
 useEffect(()=>{
@@ -48,7 +63,7 @@ useEffect(()=>{
              <p>{item.name}</p>
              <p>{item.category}</p>
              <p>{currency}{item.price}</p>
-             <p className='text-right md:text-center cursor-pointer text-lg'>X</p>
+             <p onClick={()=>removeProduct(item._id)} className='text-right md:text-center cursor-pointer text-lg'>X</p>
             </div>
           ))
         }
